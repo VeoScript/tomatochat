8639653f40f3adf9663f90570a32396f0687cd41
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-04-07 11:19:30 +0800

    style: change text-input to content-editable in chatbox

--- layouts/Panels/Chats.tsx
+++ layouts/Panels/Chats.tsx
@@ -2,10 +2,43 @@ import React from 'react'
 import AutoScroll from '@brianmcallister/react-auto-scroll'
 import Profile from '../../components/Images/Profile'
 import RoomImage from '../../components/Images/RoomImage'
+import { useForm } from 'react-hook-form'
 import { RiSettingsLine, RiSendPlane2Line } from 'react-icons/ri'
 import { messages } from '../../mock/messages'
 
+interface FormData {
+  chatbox: string
+}
+
 const Chats = () => {
+
+  const { handleSubmit, register, reset, setValue, formState: { errors, isSubmitting } } = useForm<FormData>()
+
+  React.useEffect(() => {
+    register('chatbox', { required: true })
+  }, [register])
+
+  const onSendChat = async (formData: FormData) => {
+    const chatbox = formData.chatbox
+    const contentEditable = document.getElementById('contentEditable')
+
+    if (contentEditable!.innerText.trim().length === 0 || chatbox === '') return
+
+    alert('Message: ' + formData.chatbox)
+    reset()
+
+    contentEditable !== null ?
+    contentEditable.innerHTML = '' : ''
+    contentEditable?.focus()
+  }
+
+  const handleLineBreak = (e: any) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault()
+      handleSubmit(onSendChat)()
+    }
+  }
+
   return (
     <div className="flex flex-col w-full max-w-full h-full border-x border-[#1F1836]">
       <div className="inline-flex items-center justify-between w-full p-3 border-b border-[#1F1836] bg-gradient-to-r from-[#1F1E35] to-[#14121E]">
@@ -60,20 +93,34 @@ const Chats = () => {
         </div>
       </AutoScroll>
       <div className="w-full bg-gradient-to-br from-[#1B1325] via-[#12111B] to-[#18132A]">
-        <div className="inline-flex w-full p-3 space-x-3">
-          <input
-            type="text"
-            className="w-full outline-none p-3 font-light text-xs rounded-xl bg-transparent border border-[#1F1836]"
+        <form
+          className="inline-flex w-full p-3 space-x-3"
+          onSubmit={handleSubmit(onSendChat)}
+        >
+          <div
+            id="contentEditable"
+            className="w-full h-full max-h-[15rem] overflow-y-auto cursor-text whitespace-pre-wrap outline-none p-3 font-light text-xs rounded-xl bg-transparent border border-[#1F1836]"
             placeholder="Write a message..."
+            title="Shift+Enter to execute new line."
+            contentEditable="true"
+            suppressContentEditableWarning
+            spellCheck={false}
+            onPaste={(e) => {
+              e.preventDefault()
+              var text = e.clipboardData.getData('text/plain')
+              document.execCommand('insertText', false, text)
+            }}
+            onKeyPress={handleLineBreak}
+            onInput={(e: any) => setValue('chatbox', e.currentTarget.textContent, { shouldValidate: true })}
           />
           <button
             title="Send"
-            type="button"
+            type="submit"
             className="outline-none"
           >
             <RiSendPlane2Line className="w-6 h-6 text-purple-600 transition ease-in-out duration-200 transform hover:scale-90" />
           </button>
-        </div>
+        </form>
       </div>
     </div>
   )
