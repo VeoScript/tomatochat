fbb1d64886110cdd58b218d8a8309376f5893287
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-04-18 19:09:34 +0800

    style: update discover layout layout

--- layouts/Panels/Discover.tsx
+++ layouts/Panels/Discover.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import Router from 'next/router'
 import RoomImage from '../../components/Images/RoomImage'
 import Spinner from '../../utils/Spinner'
-import { RiSearchLine, RiSpyFill } from 'react-icons/ri'
-import { useGetRooms } from '../../lib/ReactQuery'
 import { useInView } from 'react-intersection-observer'
+import { useGetRooms } from '../../lib/ReactQuery'
+import { RiSearchLine, RiSpyFill, RiEmotionSadLine, RiLockFill } from 'react-icons/ri'
 
-const DiscoverPanel: React.FC = () => {
+interface IProps {
+  user: any
+}
+
+const DiscoverPanel: React.FC<IProps> = ({ user }) => {
 
   const { ref, inView } = useInView()
   
@@ -35,28 +40,39 @@ const DiscoverPanel: React.FC = () => {
       </div>
       <div className="flex flex-col w-full h-full p-3 overflow-y-scroll scrollbar-thin scrollbar-thumb-slate-800 scrollbar-track-transparent">
         {isLoading && (
-          <div className="flex flex-col items-center justify-center w-full h-full space-y-1">
+          <div className="flex flex-col items-center justify-center w-full h-full space-y-2">
             <Spinner width={50} height={50} color={'#4D38A2'} />
             <h3 className="font-light text-sm">Loading...</h3>
           </div>
         )}
         {isError && (
-          <div className="inline-flex items-center justify-center w-full h-full space-x-1 text-sm">
-            <h3 className="font-light">Failed to load, try to</h3>
-            <button
-              type="button"
-              className="outline-none font-bold text-[#6b50d8] hover:underline"
-              onClick={() => refetch()}
-            >
-              Reload
-            </button>
+          <div className="flex flex-col items-center justify-center w-full h-full space-y-2 text-zinc-400">
+            <RiEmotionSadLine className="w-14 h-14" />
+            <div className="inline-flex items-center justify-center w-full space-x-1 text-xs">
+              <h3 className="font-light">Failed to load, try to</h3>
+              <button
+                type="button"
+                className="outline-none font-bold text-[#6b50d8] hover:underline"
+                onClick={() => refetch()}
+              >
+                Reload
+              </button>
+            </div>
           </div>
         )}
         {!isLoading && (
           <React.Fragment>
+            {rooms && rooms.pages[0].rooms.length === 0 && (
+              <div className="inline-flex items-center justify-center w-full max-w-full h-full">
+                <div className="flex flex-col">
+                  <h1 className="font-rubikglitch text-3xl text-white lowercase">tomatochat</h1>
+                  <h3 className="text-sm text-zinc-500">Welcome to TomatoChat. Discover the world of simplicity.</h3>
+                </div>
+              </div>
+            )}
             {rooms && rooms.pages.map((page: any) => (
               <React.Fragment key={page.nextId ?? 'lastPage'}>
-                {page.rooms.map((room: { photo: string, name: string, description: string }, i: number) => (
+                {page.rooms.map((room: { photo: string, name: string, slug: string, description: string, privacy: string }, i: number) => (
                   <div
                     key={i}
                     className="inline-flex w-full rounded-xl p-3 space-x-1 select-none hover:bg-gradient-to-r hover:from-[#1F1E35] hover:to-[#14121E]"
@@ -71,7 +87,10 @@ const DiscoverPanel: React.FC = () => {
                     </div>
                     <div className="inline-flex items-center justify-between w-full">
                       <div className="block w-full max-w-xs space-y-1">
-                        <h3 className="font-light text-sm">{ room.name }</h3>
+                        <span className="inline-flex items-center space-x-2">
+                          <h3 className="font-light text-sm">{ room.name }</h3>
+                          {room.privacy === 'Private' && <RiLockFill className="w-3 h-3 text-purple-500" />}
+                        </span>
                         <h3 className="font-light text-xs text-zinc-500">{ room.description }</h3>
                       </div>
                       <div className="inline-flex justify-end w-full max-w-xs space-x-3">
@@ -79,6 +98,9 @@ const DiscoverPanel: React.FC = () => {
                           title="Join"
                           type="button"
                           className="outline-none px-3 py-2 rounded-md text-xs bg-purple-800 transition ease-in-out duration-200 hover:bg-opacity-80"
+                          onClick={() => {
+                            Router.push(`${room.slug}`)
+                          }}
                         >
                           Join
                         </button>
