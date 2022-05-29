ddce175f6d68ff2cf4394eb48b1bede141e5842d
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-05-29 15:04:00 +0800

    feat: add basic seo meta tags (#54)

---
+++ components/SEO.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+interface IProps {
+  title: string
+  description: string
+  image: string
+  url: string
+}
+
+const SEO: React.FC<IProps> = ({ title, description, image, url }) => {
+  return (
+    <React.Fragment>
+      <meta name="description" content={description} />
+      <meta name="og:title" content={`${title}`} />
+      <meta name="og:image" content={`${image}`} />
+      <meta name="og:description" content={description} />
+      <meta name="og:url" content={url} />
+      <meta name="og:type" content="website" />
+    </React.Fragment>
+  )
+}
+
+export default SEO
