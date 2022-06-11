84629e8a002c1c694a16a2322161c39fef0bc945
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-06-11 20:13:29 +0800

    chore: fix optimistic updates in chat for user experience (#75)

--- lib/ReactQuery.ts
+++ lib/ReactQuery.ts
@@ -360,23 +360,23 @@ export const useSendChatMutation = () => {
   const queryClient = useQueryClient()
   return useMutation(sendChat, {
     onMutate: async (newChat) => {
-      await queryClient.cancelQueries('createChat')
-      const previousChatData = queryClient.getQueriesData('createChat')
-      queryClient.setQueriesData('createChat', (oldQueryData: any) => {
-        return {
-          ...oldQueryData,
-          data: [...oldQueryData.data, { index: oldQueryData?.data.length + 1, ...newChat }]
-        }
+      await queryClient.cancelQueries('chats')
+      const previousChatData = queryClient.getQueriesData('chats')
+      queryClient.setQueriesData('chats', (oldQueryData: any) => {
+        oldQueryData.pages.map((page: { chats: any }) => ({
+          ...page,
+          chats: [...page.chats, { index: page?.chats.length + 1, ...newChat }]
+        }))
       })
       return {
         previousChatData,
       }
     },
-    onError: (_error, _chat, context: any) => {
-      queryClient.setQueryData('createChat', context?.previousChatData)
+    onError: (context: any) => {
+      queryClient.setQueryData('chats', context?.previousChatData)
     },
     onSettled: () => {
-      queryClient.invalidateQueries('createChat')
+      queryClient.invalidateQueries('chats')
     }
   })
 }
@@ -386,23 +386,23 @@ export const useSendChatImageMutation = () => {
   const queryClient = useQueryClient()
   return useMutation(sendChatImage, {
     onMutate: async (newChat) => {
-      await queryClient.cancelQueries('createChatImage')
-      const previousChatData = queryClient.getQueriesData('createChatImage')
-      queryClient.setQueriesData('createChatImage', (oldQueryData: any) => {
-        return {
-          ...oldQueryData,
-          data: [...oldQueryData.data, { index: oldQueryData?.data.length + 1, ...newChat }]
-        }
+      await queryClient.cancelQueries('chats')
+      const previousChatData = queryClient.getQueriesData('chats')
+      queryClient.setQueriesData('chats', (oldQueryData: any) => {
+        oldQueryData.pages.map((page: { chats: any }) => ({
+          ...page,
+          chats: [...page.chats, { index: page?.chats.length + 1, ...newChat }]
+        }))
       })
       return {
         previousChatData,
       }
     },
-    onError: (_error, _chat, context: any) => {
-      queryClient.setQueryData('createChatImage', context?.previousChatData)
+    onError: (context: any) => {
+      queryClient.setQueryData('chats', context?.previousChatData)
     },
     onSettled: () => {
-      queryClient.invalidateQueries('createChatImage')
+      queryClient.invalidateQueries('chats')
     }
   })
 }
