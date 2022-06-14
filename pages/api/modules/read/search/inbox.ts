3a6fce4cd0897dabb2b123dc502fa258ec15e3dc
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-06-14 23:42:35 +0800

    chore: update inbox and discover search functionalities (#103)

--- pages/api/modules/read/search/inbox.ts
+++ pages/api/modules/read/search/inbox.ts
@@ -14,7 +14,7 @@ export default async function handler( req: NextApiRequest, res: NextApiResponse
       },
       room: {
         name: {
-          contains: req.body.searchTerm
+          startsWith: req.body.searchTerm
         }
       }
     },
