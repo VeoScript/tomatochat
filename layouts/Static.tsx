26c6061311a126b46df38f5de9f6eadbd6ed876d
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-06-14 17:55:25 +0800

    chore: add default logo in main and static layout

--- layouts/Static.tsx
+++ layouts/Static.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import TomatoChatLogo from '../utils/TomatoChatLogo'
 import { Toaster } from 'react-hot-toast'
 
 interface IProps {
@@ -15,7 +16,11 @@ const StaticLayout: React.FC<IProps> = ({ children }) => {
       <div className="flex flex-col items-center justify-center w-full h-full overflow-y-scroll scroll-smooth">
         <div className="flex flex-col items-center justify-center w-full max-w-3xl p-10 space-y-5 rounded-2xl border-0 md:border md:border-zinc-300 md:dark:border-tomato-orange">
           <div className="flex flex-col items-center w-full space-y-2 text-center">
-            <h1 className="font-rubikglitch text-5xl text-tomato-red select-none lowercase">tomato<span className="text-tomato-orange-secondary">chat</span></h1>
+            <TomatoChatLogo
+              width={50}
+              height={50}
+              fontSize="text-5xl"
+            />
             <h3 className="font-light text-sm text-zinc-800 dark:text-zinc-100">Welcome to <span className="font-bold">VEOSCRIPT's Tomato Club</span>. <br /> Start your convo, with fun and aesthetic conversations.</h3>
           </div>
           <div className="flex flex-col w-full space-y-5">
---
+++ utils/TomatoChatLogo.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Image from 'next/image'
+
+interface LogoProps {
+  width: number
+  height: number
+  fontSize: string
+}
+
+const TomatoChatLogo: React.FC<LogoProps> = ({ width, height, fontSize }) => {
+  return (
+    <div className="inline-flex items-center space-x-2">
+      <Image src="/tomatochat_svg.svg" alt="TomatoChat Logo" width={width} height={height} />
+      <span className={`font-rubikglitch ${fontSize} text-[#BD3207] lowercase`}>
+        tomato<span className="text-tomato-orange">chat</span>
+      </span>
+    </div>
+  )
+}
+
+export default TomatoChatLogo
