f4e7ef3878e9f1ac3f3ebc92fa1763b48e33822e
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-07-27 22:20:23 +0800

    feat: secure api-routes
    Secure API Routes, display only when there is a session detected.
    
    Closes #121

--- pages/api/modules/read/members.ts
+++ pages/api/modules/read/members.ts
@@ -1,7 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { getSession } from 'next-auth/react'
 import prisma from '../../../../lib/Prisma'
 
 export default async function handler( req: NextApiRequest, res: NextApiResponse) {
+
+  const session = await getSession({ req })
+
+  if (!session) {
+    res.status(401).json({ Error: 'UNAUTHENTICATED!' })
+    return
+  }
+
   const members = await prisma.joinedRoom.findMany({
     where: {
       roomSlug: req.body.roomSlug,
--- pages/api/modules/update/room.ts
+++ pages/api/modules/update/room.ts
@@ -1,9 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { getSession } from 'next-auth/react'
 import { Prisma } from '@prisma/client'
 import prisma from '../../../../lib/Prisma'
 import bcrypt from 'bcryptjs'
 
 export default async function handler( req: NextApiRequest, res: NextApiResponse) {
+
+  const session = await getSession({ req })
+
+  if (!session) {
+    res.status(401).json({ Error: 'UNAUTHENTICATED!' })
+    return
+  }
+
   try {
     if (req.method === 'POST') {
       const date = new Date()
