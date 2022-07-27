07c67d53b9f7a677ca1fd57a42cd7c73909db1a9
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-07-27 22:21:37 +0800

    feat: secure api-routes (#123)
    Secure API Routes, display only when there is a session detected.
    
    Closes #121

--- pages/api/modules/update/members/role.ts
+++ pages/api/modules/update/members/role.ts
@@ -1,7 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { getSession } from 'next-auth/react'
 import prisma from '../../../../../lib/Prisma'
 
-export default async function handler( req: NextApiRequest, res: NextApiResponse) {  
+export default async function handler( req: NextApiRequest, res: NextApiResponse) { 
+
+  const session = await getSession({ req })
+
+  if (!session) {
+    res.status(401).json({ Error: 'UNAUTHENTICATED!' })
+    return
+  }
+ 
   if (req.method === 'PUT') {
     const updateMemberRole = await prisma.joinedRoom.update({
       where: {
