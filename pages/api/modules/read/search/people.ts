0a6a45ab46c9418bc61e4a52a8a89d4c27481adb
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-10-08 17:59:14 +0800

    refactor: secure api routes for sensitive data
    Closes #143

--- pages/api/modules/read/search/people.ts
+++ pages/api/modules/read/search/people.ts
@@ -1,7 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { getSession } from 'next-auth/react'
 import prisma from '../../../../../lib/Prisma'
 
 export default async function handler( req: NextApiRequest, res: NextApiResponse) {
+  const session = await getSession({ req })
+
+  if (!session) {
+    res.status(401).json({ Error: 'UNAUTHENTICATED!' })
+    return
+  }
+  
   const searchPeople = await prisma.user.findMany({
     orderBy: {
       name: 'asc'
